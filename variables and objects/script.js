b507318dcb0715ea0420825266477ca9cb94000d
MRSE435 <150598658+MRSE435@users.noreply.github.com>
2025-11-05 12:11:52 +0530

    Add script.js with variable and object examples

---
+++ variables and objects/script.js
@@ -0,0 +1,19 @@
+var a=5;
+let b=5;
+{
+    a=6;
+    console.log(a)
+}
+{
+    let b=6;
+    console.log("b inside code block using let",b);
+}
+console.log("b after code block",b);
+console.log("javascript variables")
+let employeedetails={
+    "empname":"john",
+    "empno":"9988",
+
+}
+employeedetails.salary=900000;
+console.log(employeedetails)
